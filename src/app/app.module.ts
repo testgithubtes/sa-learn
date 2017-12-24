850c7153aaa1bee2a5eaca5a24d9497831417001
teset <so4raycpw@moakt.co>
2017-12-24 22:50:03 +0100

    Add struct general

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { CoursComponent } from './cours/cours.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MainComponent } from './layout/main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
